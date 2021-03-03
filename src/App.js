e5ff0611180399b1c8a45b22ae4e6843fcf6dbaa
nishStoryDigital <73821978+nishStoryDigital@users.noreply.github.com>
2021-03-03 15:14:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Routing from '@Components/Routing';
+
+function App() {
+    return (
+        <div className="root">
+            <Routing />
+        </div>
+    );
+}
+
+export default App;
